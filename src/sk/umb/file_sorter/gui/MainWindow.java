3527be8dd0a472739a7abf94c62ad8f4fa684bbf
Pavol Balajka <balajka.pavol@centrum.sk>
2015-10-19 17:04:37 +0200

    Create new project

---
+++ src/sk/umb/file_sorter/gui/MainWindow.java
@@ -0,0 +1,10 @@
+package sk.umb.file_sorter.gui;
+/**
+ * @author pavol
+ * @version 1.0
+ *
+ */
+
+public class MainWindow {
+
+}
---
+++ src/sk/umb/file_sorter/gui/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for GUI for file sorter
+ */
+/**
+ * @author pavol
+ *
+ */
+package sk.umb.file_sorter.gui;
